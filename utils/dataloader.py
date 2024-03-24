1376ecdb0d8bce70666c53145b4185eb9a0221c9
刘圣 <648811273@qq.com>
2024-03-24 23:15:06 +0800

    dataloader-1

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -16,25 +16,32 @@ from torch.utils.data import Dataset, DataLoader
 
 #
 class YoloDataset(Dataset):
-    def __init__(self, opt):
-        pass
+    def __init__(self, path, transform=None):
+        self.img_paths, self.label_paths = self.get_filenames(path)
+        # 设计transform模块，需要__call__()方法
+        self.transform = transform
 
     def __len__(self):
-        pass
+        return len(self.label_paths)
 
-    def __getitem__(self, index):
-        pass
+    def __getitem__(self, idx):
+        image = np.array(Image.open(self.img_paths[idx]).getdata())
+        with open(self.label_paths[idx]) as f:
+            # 训练的bbox格式为x,y,w,h
+            gt_bbox = [x.strip('\n').split('/')[-1] for x in f.readlines()]
+        sample = {'img': image, 'gt_bbox': gt_bbox}
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
 
-    def train_transforms(self):
-        pass
-
-    def val_transforms(self):
-        pass
-
-    def test_transforms(self):
-        pass
+    @staticmethod
+    def get_filenames(path):
+        sa, sb = f"{os.sep}images{os.sep}", f"{os.sep}labels{os.sep}"
+        with open(path) as f:
+            img_paths = [x.strip('\n') for x in f.readlines()]
+        label_paths = [sb.join(x.rsplit(sa, 1)).rsplit(".", 1)[0] + ".txt" for x in img_paths]
+        return img_paths, label_paths
 
 
 def yolo_collate_fn(self, batch):
     pass
-
